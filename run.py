5f565355b2b7e1d26b3e539f9d860b423a05f520
WillUnger <williamkjunger@gmail.com>
2021-10-12 15:25:13 +0000

    Adjust validation to check that all values are integers

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import gspread
-from google.oauth2.service_account import Credentials 
+from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,18 +12,19 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
-    get sales figures input from the user
+    Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market")
-    print("Data should be six numbers, separated by commas")
-    print("Example: 10, 20, 30, 40, 50, 60")
-    
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
     data_str = input("Enter your data here: ")
-    
     sales_data = data_str.split(",")
     validate_data(sales_data)
+
 
 def validate_data(values):
     """
@@ -32,6 +33,7 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
@@ -39,5 +41,5 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
 
-get_sales_data()
 
+get_sales_data()
